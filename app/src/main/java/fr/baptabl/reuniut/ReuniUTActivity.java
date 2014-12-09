014fb4e0675f7c60b8bde310a04dd19f5e19fb8a
littleDad <littledaddy@net-c.com>
2014-12-09 14:58:52 +0100

    12/09-first

--- app/src/main/java/fr/baptabl/reuniut/ReuniUTActivity.java
+++ app/src/main/java/fr/baptabl/reuniut/ReuniUTActivity.java
@@ -71,33 +71,15 @@ public class ReuniUTActivity extends ActionBarActivity implements View.OnClickLi
         Toast toast = Toast.makeText(this,strKeeplog, duration);
         toast.show();
 
-        // connexionCAS();
+
+
+
+
 
     }
 
 
-/*
-    void connexionCAS() {
-        // HttpClient httpclient = new DefaultHttpClient();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
-        HttpResponse response = client.execute(request);
 
-        String html = "";
-        InputStream in = response.getEntity().getContent();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder str = new StringBuilder();
-        String line = null;
-        while((line = reader.readLine()) != null)
-        {
-            str.append(line);
-        }
-        in.close();
-        html = str.toString();
-
-
-    }
-*/
 
 	/*
 	@Override
---
+++ app/src/main/java/fr/baptabl/reuniut/login.java
@@ -0,0 +1,37 @@
+package fr.baptabl.reuniut;
+
+/**
+ * Created by bat on 09/12/14.
+ */
+public class login {
+
+    // connexionCAS('login', 'password');
+
+    /*
+    void connexionCAS() {
+        // HttpClient httpclient = new DefaultHttpClient();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = client.execute(request);
+
+        String html = "";
+        InputStream in = response.getEntity().getContent();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder str = new StringBuilder();
+        String line = null;
+        while((line = reader.readLine()) != null)
+        {
+            str.append(line);
+        }
+        in.close();
+        html = str.toString();
+
+
+    }
+*/
+    private connect(String, String) {
+
+    }
+
+
+}
