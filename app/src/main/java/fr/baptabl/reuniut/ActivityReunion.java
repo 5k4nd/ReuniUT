6940ef58e754f646743369e3d72ab50aa7c5951d
Quentin Keunebroek <quentin.keunebroek@gmail.com>
2014-12-14 18:42:19 +0100

    commit

--- app/src/main/java/fr/baptabl/reuniut/ActivityReunion.java
+++ app/src/main/java/fr/baptabl/reuniut/ActivityReunion.java
@@ -13,13 +13,16 @@ import android.widget.TextView;
  * Created by quentinkeunebroek on 12/12/14.
  */
 public class ActivityReunion extends Activity implements View.OnClickListener {
-
+    private Button buttReunion = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent i = getIntent();
 
         setContentView(R.layout.activity_reunion);
+
+        buttReunion = (Button) findViewById(R.id.buttReunion);
+        buttReunion.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) { this.finish(); }
